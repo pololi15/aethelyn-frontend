a97cca5e538a0441f5b1c989a7e046717f96429b
pololi15 <ostinrivas15@gmail.com>
2026-08-24 01:07:09 -0400

    feat(shared): build layout structure with Navbar and Footer components

---
+++ src/app/features/home/components/services/services.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-services',
+  imports: [],
+  templateUrl: './services.html',
+  styles: ``,
+})
+export class Services {}
